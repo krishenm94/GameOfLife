ad40220564dd2abbe7cc66a478d65c22b49110a4
Krishen Mohan <krishenm@gmail.com>
2020-11-03 09:13:45 +0800

    basic implementation, took me forever to debug

--- GameOfLife.js
+++ GameOfLife.js
@@ -1,8 +1,9 @@
 let cols;
 let rows;
 let matrix;
-let nextMatrix;
-let resolution = 20;
+let resolution = 5;
+
+let PAINT_RULE = false;
 
 function makeMatrix(cols, rows) {
   let matrix = new Array(cols);
@@ -13,83 +14,100 @@ function makeMatrix(cols, rows) {
   return matrix;
 }
 
-function getNeighourhoodCount(matrix, x, y)
-{
-  let startX = i - 1 < 0 ? 0 : i -1;
-  let startY = j -1 < 0? 0 : j - 1;
-  let endX = i + 1 > matrix.length? matrix.length : i + 1;
-  let endY = j + 1 > matrix[0].length? matrix[0].length : j + 1;
-
-  let liveNeighbours = 0;
-  for (let i = startX; x <= endX; x++) {
-    for (let j = startY; y <= endY; y++) {
-      if (matrix[i][j] == 1 && !(i == x || j == y) ) {
-        liveNeighbours++;
-      }
-    }
-  }
-
-  return liveNeighbours;
-}
-
-
-function updateMatrix(matrix, nextMatrix) {
-  for (let i = 0; i < matrix.length; i++)
-  { 
-    col = matrix[i];
-    for (let j = 0; j < row.length; j++)
-    {   
-      let liveNeighbours = getNeighbourhoodCount(matrix, i, j);
-
-      // 1. Any live cell with fewer than two live neighbours dies by underpopulation
-      // 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
-      if (liveNeighbours < 2 || liveNeighbours > 3)
-      {
-        nextMatrix[i][j] = 0;
-      }
-      // 2. Any live cell with two or three live neighbours lives on to the next generation.
-      // 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-      else if ( liveNeighbours == 3) {
-        nextMatrix[i][j] = 1;
-      }
-
-      return nextMatrix;
-    }
-  }
-}
-
 function setup() {
   createCanvas(800, 800);
   cols = width / resolution;
   rows = height / resolution;
 
   matrix = makeMatrix(cols, rows);
-  nextMatrix = makeMatrix(cols, rows);
-
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       matrix[i][j] = floor(random(2));
     }
   }
-
-  background(0); 
-  stroke(0);
 }
 
 function draw() {
-
-  updateMatrix(matrix, nextMatrix);
+  background(0);
 
   for (let i = 0; i < cols; i++) {
     for (let j = 0; j < rows; j++) {
       let x = i * resolution;
       let y = j * resolution;
-      fill(matrix[i][j] % 2 == 0? 255 : 0);
-      square(x, y, resolution);
+      if (matrix[i][j] == 1)
+      {
+        fill(255);
+        rect(x, y, resolution - 1, resolution - 1);
+      }
     }
   }
 
-  let tempMatrix = matrix;
-  matrix = nextMatrix;
-  nextMatrix = tempMatrix;
+  matrix = next(matrix);
+}
+
+function next(matrix) {
+  let next = makeMatrix(cols, rows);
+
+  for (let i = 0; i < cols; i++) {    
+    for (let j = 0; j < rows; j++) {   
+      let liveNeighbours = countNeighboursWrapAround(matrix, i, j);
+
+      // 1. Any live cell with fewer than two live neighbours dies by underpopulation
+      // 3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+      if (liveNeighbours < 2 || liveNeighbours > 3)
+      {
+        fill(255, 0, 0, 100);
+        next[i][j] = 0;
+      }
+      // 2. Any live cell with two or three live neighbours lives on to the next generation.
+      // 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+      else if ( liveNeighbours == 3) {
+        fill(0, 255, 0, 100);
+        next[i][j] = 1;
+      }
+      // Otherwise copy old matrix contents
+      else {
+        fill(0, 0, 255, 100);
+        next[i][j]  = matrix[i][j];
+      }
+
+      if (PAINT_RULE)
+      {
+        ellipse(i * resolution + resolution/2, j * resolution + resolution/2, resolution, resolution);
+      }
+    }
+  }
+
+  return next;
+}
+
+function countNeighbours(matrix, i, j)
+{
+  let startX = i - 1 < 0 ? 0 : i -1;
+  let startY = j -1 < 0? 0 : j - 1;
+  let endX = i + 1 >= cols? cols - 1 : i + 1;
+  let endY = j + 1 >= rows? rows - 1 : j + 1;
+
+  let liveNeighbours = 0;
+  for (let x = startX; x <= endX; x++) {
+    for (let y = startY; y <= endY; y++) {
+      liveNeighbours += matrix[x][y];
+    }
+  }
+
+  liveNeighbours -= matrix[i][j];
+  return liveNeighbours;
+}
+
+function countNeighboursWrapAround(matrix, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+      sum += matrix[col][row];
+    }
+  }
+  sum -= matrix[x][y];
+  return sum;
 }
